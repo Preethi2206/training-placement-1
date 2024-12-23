b58c3252af4dd8085ef2ce2f356c164938a32750
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2024-12-23 19:39:20 +0530

    10-hackerrank.java

---
+++ 23.12.2024/10-hackerrank.java
@@ -0,0 +1,16 @@
+class Singleton {
+    private static Singleton instance;
+    private Singleton() {}
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+public class SingletonTest {
+    public static void main(String[] args) {
+        Singleton obj = Singleton.getInstance();
+        System.out.println("Singleton Instance: " + obj);
+    }
+}
