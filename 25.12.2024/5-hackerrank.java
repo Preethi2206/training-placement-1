4bad566423d006f1b6f11b44e6f7061f0ddd54d1
Preethi2206 <147687615+Preethi2206@users.noreply.github.com>
2024-12-25 18:40:54 +0530

    5-hackerrank.java

---
+++ 25.12.2024/5-hackerrank.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1; 
+        while (scanner.hasNext()) {
+            System.out.println(lineNumber + " " + scanner.nextLine());
+            lineNumber++; 
+        }
+        scanner.close(); 
+    }
+}
